5fc9e8b3825272e102a9bd06f509aecf4c02fa94
Arathi <arathinambiar67@gmail.com>
2025-06-25 22:00:17 +0530

    first working stage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected title = 'vehicle_insurance';
-}
+export { App } from './newPolicy/newPolicy';
